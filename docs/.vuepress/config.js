e99158c52f2efc30b08c0d95123d2ac29dde8747
yanyue404 <xiaoyueyue165@gmail.com>
2023-10-19 19:30:29 +0800

    docs: base url

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,81 +1,72 @@
 module.exports = {
-  title: 'fe-dev-playbook',
+  title: "fe-dev-playbook",
+  base: "/fe-dev-playbook/",
   head: [
     [
-      'link',
+      "link",
       {
-        rel: 'icon',
-        href: 'https://avatars1.githubusercontent.com/u/50347314?s=200&v=4',
+        rel: "icon",
+        href: "https://avatars1.githubusercontent.com/u/24217900?s=200&v=4",
       },
     ],
     [
-      'meta',
+      "meta",
       {
-        name: 'keywords',
-        itemprop: 'keywords',
-        content: '打造舒适的前端开发环境',
-      },
-    ],
-    ['meta', {
-      property: 'og:title',
-      content: 'fe-dev-playbook'
-    }],
-    [
-      'meta',
-      {
-        property: 'og:description',
-        content: '前端开发环境',
+        name: "keywords",
+        itemprop: "keywords",
+        content: "打造舒适的前端开发环境",
       },
     ],
     [
-      'script',
-      {},
-      `var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?623b47a95299ebf6b01c952b9ec44456";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })()`,
+      "meta",
+      {
+        property: "og:title",
+        content: "fe-dev-playbook",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:description",
+        content: "前端开发环境",
+      },
     ],
   ],
-  description: '打造舒适高效的前端开发环境',
+  description: "打造舒适高效的前端开发环境",
   themeConfig: {
-    nav: [{
-        text: '指南',
-        link: '/guide/'
+    nav: [
+      {
+        text: "指南",
+        link: "/guide/",
       },
       {
-        text: 'Github',
-        link: 'https://github.com/ykfe/fe-dev-playbook'
-      },
-      {
-        text: 'SSR 框架',
-        link: 'http://doc.ssr-fc.com/'
+        text: "Github",
+        link: "https://github.com/yanyue404/fe-dev-playbook",
       },
     ],
     sidebar: {
-      '/guide/': [{
-        collapsable: false,
-        children: [
-          'Shell',
-          'Nodejs',
-          'VSCode',
-          'BreakingPoint',
-          'Ts',
-          'Git',
-          'Chrome',
-          'Fe',
-          'Vim',
-          'Mac',
-          'Command',
-          'Utils',
-          'Tips',
-          'Algorithm',
-          'Github'
-        ]
-      }]
-    }
-
-  }
+      "/guide/": [
+        {
+          collapsable: false,
+          children: [
+            "Shell",
+            "Nodejs",
+            "VSCode",
+            "BreakingPoint",
+            "Ts",
+            "Git",
+            "Chrome",
+            "Fe",
+            "Vim",
+            "Mac",
+            "Command",
+            "Utils",
+            "Tips",
+            "Algorithm",
+            "Github",
+          ],
+        },
+      ],
+    },
+  },
 };
